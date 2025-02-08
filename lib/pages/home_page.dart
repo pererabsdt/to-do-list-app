3c875914fbda88783376d2f37b8a4a1e0dd0e849
Dulitha Perera <dulitha.22@cse.mrt.ac.lk>
2025-02-08 12:04:03 +0530

    creating to_do_tile

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app/util/to_do_tile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,16 +12,14 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.yellow,
-      appBar: AppBar(
-        title: Center(child: Text('To Do')),
-        elevation: 0,
-      ),
-      body: ListView(
-        children: [
-          
-        ]
-      )
-    );
+        backgroundColor: Colors.yellow[300],
+        appBar: AppBar(
+          backgroundColor: Colors.yellow[600],
+          title: Center(child: Text('To Do')),
+          elevation: 0,
+        ),
+        body: ListView(children: [
+          ToDoTile(),
+        ]));
   }
 }
--- lib/util/to_do_tile.dart
+++ lib/util/to_do_tile.dart
@@ -1,1 +1,16 @@
 import 'package:flutter/material.dart';
+
+class ToDoTile extends StatelessWidget {
+  const ToDoTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: Container(
+        child: Text("make tutorial"),
+        decoration: BoxDecoration(color: Colors.yellow[600]),
+      ),
+    );
+  }
+}
