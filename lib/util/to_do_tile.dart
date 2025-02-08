9bf82cdebceb05422ac3a517a91fa92c9987b160
Dulitha Perera <dulitha.22@cse.mrt.ac.lk>
2025-02-08 12:19:13 +0530

    added fields for to do tile

--- lib/util/to_do_tile.dart
+++ lib/util/to_do_tile.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
 
 class ToDoTile extends StatelessWidget {
-  const ToDoTile({super.key});
+  final String taskName;
+  final bool taskCompleted;
+  Function(bool?)? onChanged;
+
+  const ToDoTile({
+    super.key,
+    required this.taskName,
+    required this.taskCompleted,
+    required this.onChanged,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(25.0),
       child: Container(
-        child: Text("make tutorial"),
-        decoration: BoxDecoration(color: Colors.yellow[600]),
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: Colors.yellow[600],
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          children: [
+            Checkbox(value: taskCompleted, onChanged: onChanged),
+            Text(taskName),
+          ],
+        ),
       ),
     );
   }
