40235cddbadf6b409e7a87e9c9b04496f3c4b5c5
Dulitha Perera <dulitha.22@cse.mrt.ac.lk>
2025-02-08 12:34:16 +0530

    change padding

--- lib/util/to_do_tile.dart
+++ lib/util/to_do_tile.dart
@@ -15,7 +15,7 @@ class ToDoTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(25.0),
+      padding: const EdgeInsets.only(left: 25, right: 25, top: 25),
       child: Container(
         padding: const EdgeInsets.all(20),
         decoration: BoxDecoration(
